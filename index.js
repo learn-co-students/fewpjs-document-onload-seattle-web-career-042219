64db1914c9f1da925a2cf2cee5c5dad54dd13a4e
github username <github email address>
2019-06-05 11:28:23 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Your code goes here
+document.addEventListener('DOMContentLoaded', function() {
+    editText();
+});
+
+function editText() {
+    document.getElementById("text").textContent = "This is really cool!";
+}
